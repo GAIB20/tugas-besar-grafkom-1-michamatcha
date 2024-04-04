07bf2b7a12e90ff10a45545840fce6ef38e5f08e
Kandida Gunawangit config --global user.name Kandida <kandidagunawan@gmail.com>
2024-04-04 07:54:55 +0700

    feat: select, translation

--- src/model/rectangle.ts
+++ src/model/rectangle.ts
@@ -3,6 +3,8 @@ import { getColor } from "../utils/colorUtil";
 
 
 class Rectangle implements Drawable{
+
+
     initialPoint : Point
     secondPoint : Point
     vertices: number[] = []
@@ -19,7 +21,6 @@ class Rectangle implements Drawable{
     setColors(colors : [number, number, number, number]){
         this.colors = colors
     }
-
     
     setAllPointsByInput(_point1: Point, _point2: Point){
         this.vertices = [
@@ -31,7 +32,19 @@ class Rectangle implements Drawable{
             this.initialPoint.x, this.secondPoint.y, ...getColor()   
         ];
     }
+    translate(_deltaX: number, _deltaY: number) {
+        this.initialPoint.moveCoordinateX(_deltaX)
+        this.secondPoint.moveCoordinateX(_deltaX)
+        this.initialPoint.moveCoordinateY(_deltaY)
+        this.secondPoint.moveCoordinateY(_deltaY)
+        this.setAllPointsByInput(this.initialPoint, this.secondPoint)
+    }
 
+
+    isCoordInside(coord: [number, number]): boolean {
+        return (coord[0] >= Math.min(this.initialPoint.x, this.secondPoint.x) && coord[0] <= Math.max(this.initialPoint.x, this.secondPoint.x)&&
+        coord[1] >= Math.min(this.initialPoint.y, this.secondPoint.y) && coord[1] <= Math.max(this.initialPoint.y, this.secondPoint.y)) 
+    }
     draw(gl: WebGLRenderingContext): void {
         gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(this.vertices), gl.STATIC_DRAW)
         gl.drawArrays(gl.TRIANGLE_FAN, 0, 6);
